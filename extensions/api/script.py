3e124758d1b1e56a9975f6142f5540cd568cb8f9
Swapnil De Sarkar <37899914+ItzSwapnil@users.noreply.github.com>
2024-04-21 02:52:33 +0530

    improve

--- extensions/api/script.py
+++ extensions/api/script.py
@@ -2,7 +2,16 @@ import extensions.api.blocking_api as blocking_api
 import extensions.api.streaming_api as streaming_api
 from modules import shared
 
+def start_api_servers():
+    blocking_port = get_blocking_port()
+    streaming_port = get_streaming_port()
 
-def setup():
-    blocking_api.start_server(shared.args.api_blocking_port, share=shared.args.public_api)
-    streaming_api.start_server(shared.args.api_streaming_port, share=shared.args.public_api)
+    blocking_api.start_server(blocking_port, share=shared.args.public_api)
+    streaming_api.start_server(streaming_port, share=shared.args.public_api)
+
+def get_blocking_port():
+    blocking_port = shared.args.api_blocking_port
+    if not blocking_port:
+        blocking_port = 8000  # Default port
+    return blocking_port
+
