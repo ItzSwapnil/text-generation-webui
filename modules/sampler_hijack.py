e962d677672e3ac3518bf456aab4ed3bff29a30c
Swapnil De Sarkar <37899914+ItzSwapnil@users.noreply.github.com>
2024-04-21 02:52:58 +0530

    improve

--- modules/sampler_hijack.py
+++ modules/sampler_hijack.py
@@ -1,14 +1,10 @@
 import torch
 import transformers
-from transformers import LogitsWarper
-from transformers.generation.logits_process import LogitNormalization, LogitsProcessorList
-
+from transformers import LogitsWarper, LogitsProcessorList, LogitNormalization
 
 class TailFreeLogitsWarper(LogitsWarper):
     def __init__(self, tfs: float, filter_value: float = -float("Inf"), min_tokens_to_keep: int = 1):
-        tfs = float(tfs)
-        if tfs < 0 or tfs > 1.0:
-            raise ValueError(f"`tfs` has to be a float >= 0 and <= 1, but is {tfs}")
+        super().__init__()
         self.tfs = tfs
         self.filter_value = filter_value
         self.min_tokens_to_keep = min_tokens_to_keep
@@ -17,15 +13,12 @@ class TailFreeLogitsWarper(LogitsWarper):
         sorted_logits, sorted_indices = torch.sort(scores, descending=True)
         probs = sorted_logits.softmax(dim=-1)
 
-        # Compute second derivative normalized CDF
         d2 = probs.diff().diff().abs()
         normalized_d2 = d2 / d2.sum(dim=-1, keepdim=True)
         normalized_d2_cdf = normalized_d2.cumsum(dim=-1)
 
-        # Remove tokens with CDF value above the threshold (token with 0 are kept)
-        sorted_indices_to_remove = normalized_d2_cdf > self.tfs
+        sorted_indices_to_remove = (normalized_d2_cdf > self.tfs) & (sorted_indices != 0)
 
-        # Centre the distribution around the cutoff as in the original implementation of the algorithm
         sorted_indices_to_remove = torch.cat(
             (
                 torch.zeros(scores.shape[0], 1, dtype=torch.bool, device=scores.device),
@@ -36,19 +29,15 @@ class TailFreeLogitsWarper(LogitsWarper):
         )
 
         if self.min_tokens_to_keep > 1:
-            # Keep at least min_tokens_to_keep
-            sorted_indices_to_remove[..., : self.min_tokens_to_keep] = 0
+            sorted_indices_to_remove[..., :self.min_tokens_to_keep] = 0
 
         indices_to_remove = sorted_indices_to_remove.scatter(1, sorted_indices, sorted_indices_to_remove)
         scores = scores.masked_fill(indices_to_remove, self.filter_value)
         return scores
 
-
 class TopALogitsWarper(LogitsWarper):
     def __init__(self, top_a: float, filter_value: float = -float("Inf"), min_tokens_to_keep: int = 1):
-        top_a = float(top_a)
-        if top_a < 0 or top_a > 1.0:
-            raise ValueError(f"`top_a` has to be a float >= 0 and <= 1, but is {top_a}")
+        super().__init__()
         self.top_a = top_a
         self.filter_value = filter_value
         self.min_tokens_to_keep = min_tokens_to_keep
@@ -57,21 +46,18 @@ class TopALogitsWarper(LogitsWarper):
         sorted_logits, sorted_indices = torch.sort(scores, descending=True)
         probs = sorted_logits.softmax(dim=-1)
 
-        # Remove tokens with probability less than top_a*(max(probs))^2 (token with 0 are kept)
         probs_max = probs[..., 0, None]
         sorted_indices_to_remove = probs < probs_max * probs_max * self.top_a
 
         if self.min_tokens_to_keep > 1:
-            # Keep at least min_tokens_to_keep
-            sorted_indices_to_remove[..., : self.min_tokens_to_keep] = 0
+            sorted_indices_to_remove[..., :self.min_tokens_to_keep] = 0
 
         indices_to_remove = sorted_indices_to_remove.scatter(1, sorted_indices, sorted_indices_to_remove)
         scores = scores.masked_fill(indices_to_remove, self.filter_value)
         return scores
 
-
 def get_logits_warper_patch(self, generation_config):
-    warpers = self._get_logits_warper_old(generation_config)
+    warpers = self._get_logits_warper(generation_config)
     warpers_to_add = LogitsProcessorList()
     min_tokens_to_keep = 2 if generation_config.num_beams > 1 else 1
 
@@ -87,16 +73,10 @@ def get_logits_warper_patch(self, generation_config):
 
     return warpers
 
-
 def generation_config_init_patch(self, **kwargs):
-    self.__init___old(**kwargs)
+    super().__init__(**kwargs)
     self.tfs = kwargs.pop("tfs", 1.0)
     self.top_a = kwargs.pop("top_a", 0.0)
 
-
-def hijack_samplers():
-    transformers.GenerationMixin._get_logits_warper_old = transformers.GenerationMixin._get_logits_warper
-    transformers.GenerationMixin._get_logits_warper = get_logits_warper_patch
-
-    transformers.GenerationConfig.__init___old = transformers.GenerationConfig.__init__
-    transformers.GenerationConfig.__init__ = generation_config_init_patch
+transformers.GenerationMixin._get_logits_warper = get_logits_warper_patch
+transformers.GenerationConfig.__init__ = generation_config_init_patch
