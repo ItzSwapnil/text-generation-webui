cafc66e93e92e6e619d26a5d29b4f3df4d5d4ccb
Swapnil De Sarkar <37899914+ItzSwapnil@users.noreply.github.com>
2024-04-21 02:52:51 +0530

    improve

--- extensions/elevenlabs_tts/script.py
+++ extensions/elevenlabs_tts/script.py
@@ -1,97 +1,102 @@
+import os
 import re
 from pathlib import Path
+import traceback
 
-import elevenlabs
 import gradio as gr
-from modules import chat, shared
+import elevenlabs  # type: ignore
+from modules import chat, shared  # type: ignore
 
 params = {
-    'activate': True,
-    'api_key': None,
-    'selected_voice': 'None',
-    'autoplay': False,
-    'show_text': True,
+    "activate": True,
+    "api_key": None,
+    "selected_voice": "None",
+    "autoplay": False,
+    "show_text": True,
 }
 
 voices = None
 wav_idx = 0
 
 
-def update_api_key(key):
-    params['api_key'] = key
-    if key is not None:
+def update_api_key(key: str) -> None:
+    params["api_key"] = key
+    if key is not None and elevenlabs:
         elevenlabs.set_api_key(key)
 
 
-def refresh_voices():
-    global params
-    your_voices = elevenlabs.voices()
-    voice_names = [voice.name for voice in your_voices]
-    return voice_names
+def refresh_voices() -> list[str]:
+    if not elevenlabs:
+        return []
+    return [voice.name for voice in elevenlabs.voices()]
 
 
-def refresh_voices_dd():
+def refresh_voices_dd() -> gr.Dropdown:
     all_voices = refresh_voices()
-    return gr.Dropdown.update(value=all_voices[0], choices=all_voices)
+    return gr.Dropdown.update(value=all_voices[0], choices=all_voices) if all_voices else gr.Dropdown()
 
 
-def remove_tts_from_history():
-    for i, entry in enumerate(shared.history['internal']):
-        shared.history['visible'][i] = [shared.history['visible'][i][0], entry[1]]
+def remove_tts_from_history() -> None:
+    if not shared:
+        return
+    for i, entry in enumerate(shared.history["internal"]):
+        shared.history["visible"][i] = [shared.history["visible"][i][0], entry[1]]
 
 
-def toggle_text_in_history():
-    for i, entry in enumerate(shared.history['visible']):
+def toggle_text_in_history() -> None:
+    if not shared:
+        return
+    for i, entry in enumerate(shared.history["visible"]):
         visible_reply = entry[1]
-        if visible_reply.startswith('<audio'):
-            if params['show_text']:
-                reply = shared.history['internal'][i][1]
-                shared.history['visible'][i] = [
-                    shared.history['visible'][i][0], f"{visible_reply.split('</audio>')[0]}</audio>\n\n{reply}"
+        if visible_reply.startswith("<audio"):
+            if params["show_text"]:
+                reply = shared.history["internal"][i][1]
+                shared.history["visible"][i] = [
+                    shared.history["visible"][i][0], f"{visible_reply.split('</audio>')[0]}</audio>\n\n{reply}"
                 ]
             else:
-                shared.history['visible'][i] = [
-                    shared.history['visible'][i][0], f"{visible_reply.split('</audio>')[0]}</audio>"
+                shared.history["visible"][i] = [
+                    shared.history["visible"][i][0], f"{visible_reply.split('</audio>')[0]}</audio>"
                 ]
 
 
-def remove_surrounded_chars(string):
+def remove_surrounded_chars(string: str) -> str:
     # this expression matches to 'as few symbols as possible (0 upwards) between any asterisks' OR
     # 'as few symbols as possible (0 upwards) between an asterisk and the end of the string'
-    return re.sub('\*[^\*]*?(\*|$)', '', string)
+    return re.sub("\*[^\*]*?(\*|$)", "", string)
 
 
-def state_modifier(state):
-    if not params['activate']:
+def state_modifier(state: dict) -> dict:
+    if not params["activate"]:
         return state
 
-    state['stream'] = False
+    state["stream"] = False
     return state
 
 
-def input_modifier(string):
-    if not params['activate']:
+def input_modifier(string: str) -> str:
+    if not params["activate"]:
         return string
 
     shared.processing_message = "*Is recording a voice message...*"
     return string
 
 
-def history_modifier(history):
+def history_modifier(history: dict) -> dict:
     # Remove autoplay from the last reply
-    if len(history['internal']) > 0:
-        history['visible'][-1] = [
-            history['visible'][-1][0],
-            history['visible'][-1][1].replace('controls autoplay>', 'controls>')
+    if len(history["internal"]) > 0:
+        history["visible"][-1] = [
+            history["visible"][-1][0],
+            history["visible"][-1][1].replace('controls autoplay>', 'controls>')
         ]
 
     return history
 
 
-def output_modifier(string):
+def output_modifier(string: str) -> str:
     global params, wav_idx
 
-    if not params['activate']:
+    if not params["activate"]:
         return string
 
     original_string = string
@@ -106,10 +111,10 @@ def output_modifier(string):
     output_file = Path(f'extensions/elevenlabs_tts/outputs/{wav_idx:06d}.mp3'.format(wav_idx))
     print(f'Outputting audio to {str(output_file)}')
     try:
-        audio = elevenlabs.generate(text=string, voice=params['selected_voice'], model="eleven_monolingual_v1")
+        audio = elevenlabs.generate(text=string, voice=params["selected_voice"], model="eleven_monolingual_v1")
         elevenlabs.save(audio, str(output_file))
 
-        autoplay = 'autoplay' if params['autoplay'] else ''
+        autoplay = 'autoplay' if params["autoplay"] else ''
         string = f'<audio src="file/{output_file.as_posix()}" controls {autoplay}></audio>'
         wav_idx += 1
     except elevenlabs.api.error.UnauthenticatedRateLimitError:
@@ -118,63 +123,19 @@ def output_modifier(string):
         string = "🤖 ElevenLabs API Tier Limit Reached\n\n"
     except elevenlabs.api.error.APIError as err:
         string = f"🤖 ElevenLabs Error: {err}\n\n"
+    except Exception as e:
+        traceback.print_exc()
+        string = f"🤖 Unexpected Error: {str(e)}\n\n"
 
-    if params['show_text']:
+    if params["show_text"]:
         string += f'\n\n{original_string}'
 
     shared.processing_message = "*Is typing...*"
     return string
 
 
-def ui():
+def ui() -> gr.Blocks:
     global voices
     if not voices:
         voices = refresh_voices()
-        params['selected_voice'] = voices[0]
-
-    # Gradio elements
-    with gr.Row():
-        activate = gr.Checkbox(value=params['activate'], label='Activate TTS')
-        autoplay = gr.Checkbox(value=params['autoplay'], label='Play TTS automatically')
-        show_text = gr.Checkbox(value=params['show_text'], label='Show message text under audio player')
-
-    with gr.Row():
-        voice = gr.Dropdown(value=params['selected_voice'], choices=voices, label='TTS Voice')
-        refresh = gr.Button(value='Refresh')
-
-    with gr.Row():
-        api_key = gr.Textbox(placeholder="Enter your API key.", label='API Key')
-
-    with gr.Row():
-        convert = gr.Button('Permanently replace audios with the message texts')
-        convert_cancel = gr.Button('Cancel', visible=False)
-        convert_confirm = gr.Button('Confirm (cannot be undone)', variant="stop", visible=False)
-
-    # Convert history with confirmation
-    convert_arr = [convert_confirm, convert, convert_cancel]
-    convert.click(lambda: [gr.update(visible=True), gr.update(visible=False), gr.update(visible=True)], None, convert_arr)
-    convert_confirm.click(
-        lambda: [gr.update(visible=False), gr.update(visible=True), gr.update(visible=False)], None, convert_arr).then(
-        remove_tts_from_history, None, None).then(
-        chat.save_history, shared.gradio['mode'], None, show_progress=False).then(
-        chat.redraw_html, shared.reload_inputs, shared.gradio['display'])
-
-    convert_cancel.click(lambda: [gr.update(visible=False), gr.update(visible=True), gr.update(visible=False)], None, convert_arr)
-
-    # Toggle message text in history
-    show_text.change(
-        lambda x: params.update({"show_text": x}), show_text, None).then(
-        toggle_text_in_history, None, None).then(
-        chat.save_history, shared.gradio['mode'], None, show_progress=False).then(
-        chat.redraw_html, shared.reload_inputs, shared.gradio['display'])
-
-    convert_cancel.click(lambda: [gr.update(visible=False), gr.update(visible=True), gr.update(visible=False)], None, convert_arr)
-
-    # Event functions to update the parameters in the backend
-    activate.change(lambda x: params.update({'activate': x}), activate, None)
-    voice.change(lambda x: params.update({'selected_voice': x}), voice, None)
-    api_key.change(update_api_key, api_key, None)
-    # connect.click(check_valid_api, [], connection_status)
-    refresh.click(refresh_voices_dd, [], voice)
-    # Event functions to update the parameters in the backend
-    autoplay.change(lambda x: params.update({"autoplay": x}), autoplay, None)
+        params["selected_
