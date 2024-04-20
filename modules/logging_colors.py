54d90dee729eb42706ea2803de13117b8b76d6dc
Swapnil De Sarkar <37899914+ItzSwapnil@users.noreply.github.com>
2024-04-21 02:53:08 +0530

    improve

--- modules/logging_colors.py
+++ modules/logging_colors.py
@@ -1,114 +1,82 @@
-# Copied from https://stackoverflow.com/a/1336640
-
 import logging
 import platform
+import ctypes
+import colorama  # Windows console coloring
+import termcolor  # Non-Windows console coloring
 
-logging.basicConfig(format='%(levelname)s:%(message)s')
+# Define constants for the different colors
+COLOR_BLACK = 0x0000
+COLOR_BLUE = 0x0001
+COLOR_GREEN = 0x0002
+COLOR_CYAN = 0x0003
+COLOR_RED = 0x0004
+COLOR_MAGENTA = 0x0005
+COLOR_YELLOW = 0x0006
+COLOR_WHITE = 0x0007
+COLOR_INTENSITY = 0x0008
 
+# Define a constant to reset the color to the default
+RESET_COLOR = COLOR_WHITE
 
-def add_coloring_to_emit_windows(fn):
-    # add methods we need to the class
-    def _out_handle(self):
-        import ctypes
-        return ctypes.windll.kernel32.GetStdHandle(self.STD_OUTPUT_HANDLE)
-    out_handle = property(_out_handle)
+# Define a class to handle colored logging on Windows
+class ColorHandler(logging.StreamHandler):
+    def __init__(self, stream=None):
+        super().__init__(stream=stream)
+        self.out_handle = ctypes.windll.kernel32.GetStdHandle(-11)
 
-    def _set_color(self, code):
-        import ctypes
+    def emit(self, record):
+        levelno = record.levelno
+        color = None
 
-        # Constants from the Windows API
-        self.STD_OUTPUT_HANDLE = -11
-        hdl = ctypes.windll.kernel32.GetStdHandle(self.STD_OUTPUT_HANDLE)
-        ctypes.windll.kernel32.SetConsoleTextAttribute(hdl, code)
-
-    setattr(logging.StreamHandler, '_set_color', _set_color)
-
-    def new(*args):
-        FOREGROUND_BLUE = 0x0001  # text color contains blue.
-        FOREGROUND_GREEN = 0x0002  # text color contains green.
-        FOREGROUND_RED = 0x0004  # text color contains red.
-        FOREGROUND_INTENSITY = 0x0008  # text color is intensified.
-        FOREGROUND_WHITE = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED
-        # winbase.h
-        # STD_INPUT_HANDLE = -10
-        # STD_OUTPUT_HANDLE = -11
-        # STD_ERROR_HANDLE = -12
-
-        # wincon.h
-        # FOREGROUND_BLACK = 0x0000
-        FOREGROUND_BLUE = 0x0001
-        FOREGROUND_GREEN = 0x0002
-        # FOREGROUND_CYAN = 0x0003
-        FOREGROUND_RED = 0x0004
-        FOREGROUND_MAGENTA = 0x0005
-        FOREGROUND_YELLOW = 0x0006
-        # FOREGROUND_GREY = 0x0007
-        FOREGROUND_INTENSITY = 0x0008  # foreground color is intensified.
-
-        # BACKGROUND_BLACK = 0x0000
-        # BACKGROUND_BLUE = 0x0010
-        # BACKGROUND_GREEN = 0x0020
-        # BACKGROUND_CYAN = 0x0030
-        # BACKGROUND_RED = 0x0040
-        # BACKGROUND_MAGENTA = 0x0050
-        BACKGROUND_YELLOW = 0x0060
-        # BACKGROUND_GREY = 0x0070
-        BACKGROUND_INTENSITY = 0x0080  # background color is intensified.
-
-        levelno = args[1].levelno
-        if (levelno >= 50):
-            color = BACKGROUND_YELLOW | FOREGROUND_RED | FOREGROUND_INTENSITY | BACKGROUND_INTENSITY
-        elif (levelno >= 40):
-            color = FOREGROUND_RED | FOREGROUND_INTENSITY
-        elif (levelno >= 30):
-            color = FOREGROUND_YELLOW | FOREGROUND_INTENSITY
-        elif (levelno >= 20):
-            color = FOREGROUND_GREEN
-        elif (levelno >= 10):
-            color = FOREGROUND_MAGENTA
+        if levelno >= 50:
+            color = COLOR_RED | COLOR_INTENSITY
+        elif levelno >= 40:
+            color = COLOR_RED | COLOR_INTENSITY
+        elif levelno >= 30:
+            color = COLOR_YELLOW | COLOR_INTENSITY
+        elif levelno >= 20:
+            color = COLOR_GREEN
+        elif levelno >= 10:
+            color = COLOR_MAGENTA
         else:
-            color = FOREGROUND_WHITE
-        args[0]._set_color(color)
+            color = COLOR_WHITE
 
-        ret = fn(*args)
-        args[0]._set_color(FOREGROUND_WHITE)
-        # print "after"
-        return ret
-    return new
+        if color is not None:
+            if not ctypes.windll.kernel32.SetConsoleTextAttribute(self.out_handle, color):
+                # If the function fails, print an error message and reset the color to the default
+                print(f"Error setting console text attribute to {color}: {ctypes.FormatError()}")
+                color = RESET_COLOR
 
+        super().emit(record)
 
-def add_coloring_to_emit_ansi(fn):
-    # add methods we need to the class
-    def new(*args):
-        levelno = args[1].levelno
-        if (levelno >= 50):
-            color = '\x1b[31m'  # red
-        elif (levelno >= 40):
-            color = '\x1b[31m'  # red
-        elif (levelno >= 30):
-            color = '\x1b[33m'  # yellow
-        elif (levelno >= 20):
-            color = '\x1b[32m'  # green
-        elif (levelno >= 10):
-            color = '\x1b[35m'  # pink
-        else:
-            color = '\x1b[0m'  # normal
-        args[1].msg = color + args[1].msg + '\x1b[0m'  # normal
-        # print "after"
-        return fn(*args)
-    return new
+        if color is not None:
+            # Reset the color to the default
+            ctypes.windll.kernel32.SetConsoleTextAttribute(self.out_handle, RESET_COLOR)
 
+# Define a function to set up the logger
+def setup_logging():
+    # Create a logger
+    logger = logging.getLogger('text-generation-webui')
+    logger.setLevel(logging.DEBUG)
 
-if platform.system() == 'Windows':
-    # Windows does not support ANSI escapes and we are using API calls to set the console color
-    logging.StreamHandler.emit = add_coloring_to_emit_windows(logging.StreamHandler.emit)
-else:
-    # all non-Windows platforms are supporting ANSI escapes so we use them
-    logging.StreamHandler.emit = add_coloring_to_emit_ansi(logging.StreamHandler.emit)
-    # log = logging.getLogger()
-    # log.addFilter(log_filter())
-    # //hdlr = logging.StreamHandler()
-    # //hdlr.setFormatter(formatter())
+    # Create a console handler with coloring
+    if platform.system() == 'Windows':
+        # Use the ColorHandler class on Windows
+        handler = ColorHandler()
+    else:
+        # Use the termcolor library on non-Windows platforms
+        handler = logging.StreamHandler()
+        handler.setFormatter(termcolor.colorize_formatter(logger.handlers[0].formatter))
 
-logger = logging.getLogger('text-generation-webui')
-logger.setLevel(logging.DEBUG)
+    # Add the handler to the logger
+    logger.addHandler(handler)
+
+# Call the setup_logging function to set up the logger
+setup_logging()
+
+# Test the logger
+logger.debug("This is a debug message")
+logger.info("This is an info message")
+logger.warning("This is a warning message")
+logger.error("This is an error message")
+logger.critical("This is a critical message")
